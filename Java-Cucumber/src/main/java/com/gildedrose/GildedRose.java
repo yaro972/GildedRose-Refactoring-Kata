36ccfe3259a9d520ff8aeb2c0c66a73dd6e72ceb
Thierry Aronoff <thierry.aronoff@gmail.com>
2024-07-08 14:19:20 +0200

    [FIX]: Refactoring code

--- Java-Cucumber/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Cucumber/src/main/java/com/gildedrose/GildedRose.java
@@ -1,62 +1,76 @@
 package com.gildedrose;
 
 class GildedRose {
-    Item[] items;
+  static final String AGED_BRIE = "Aged Brie";
+  static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+  static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+  static final int MAX_QUALITY = 50;
+  static final int MIN_QUALITY = 0;
+  private static final int DAYS_UNTIL_EXPIRATION = 6;
+  private static final int EXPIRATION_THRESHOLD = 11;
+  public static final int MIN_SELL_IN = 0;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
-    }
+  Item[] items;
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+  public GildedRose(Item[] items) {
+    this.items = items;
+  }
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+  public void updateQuality() {
+    for (Item item : items) {
+      if (isAgedBrie(item) || isBackstage(item)) {
+        increaseQuality(item);
+        if (isBackstage(item)) {
+          if (item.sellIn < EXPIRATION_THRESHOLD) {
+            increaseQuality(item);
+          }
+          if (item.sellIn < DAYS_UNTIL_EXPIRATION) {
+            increaseQuality(item);
+          }
         }
+      } else {
+        decreaseQuality(item);
+      }
+
+      if (isNotSulfuras(item)) {
+        item.sellIn--;
+      }
+
+      if (item.sellIn < MIN_SELL_IN) {
+        if (isAgedBrie(item) && item.quality < MAX_QUALITY) {
+          item.quality--;
+        } else if (isBackstage(item)) {
+          item.quality = MIN_QUALITY;
+        } else if (!isAgedBrie(item) && item.quality > MAX_QUALITY) {
+          decreaseQuality(item);
+        }
+      }
     }
+  }
+
+
+  private boolean isBackstage(Item item) {
+    return item.name.equals(BACKSTAGE);
+  }
+
+  private boolean isNotSulfuras(Item item) {
+    return !item.name.equals(SULFURAS);
+  }
+
+  private boolean isAgedBrie(Item item) {
+    return item.name.equals(AGED_BRIE);
+  }
+
+  private void increaseQuality(Item item) {
+    if (item.quality < MAX_QUALITY) {
+      item.quality++;
+    }
+  }
+
+  private void decreaseQuality(Item item) {
+    if (item.quality > MIN_QUALITY && isNotSulfuras(item)) {
+      item.quality--;
+    }
+  }
+
 }
